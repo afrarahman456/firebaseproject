01083a53b6a624bd5f2dde062993b60acb49d673
afrazeharin <afrarafah@gmail.com>
2025-09-10 13:09:21 +0400

    firstcommit

--- lib/todoapp.dart
+++ lib/todoapp.dart
@@ -1,6 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+
 
 class Todoapp extends StatefulWidget {
   const Todoapp({super.key});
@@ -10,44 +11,64 @@ class Todoapp extends StatefulWidget {
 }
 
 class _TodoappState extends State<Todoapp> {
+  TextEditingController _controller = TextEditingController();
+  TextEditingController controller =TextEditingController();
+  final _formkey=GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Todo App ")),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: "Enter a todo",
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                ),
-                SizedBox(width: 8),
-                ElevatedButton(onPressed: () {}, child: Text("Add")),
-              ],
-            ),
-            SizedBox(height: 30,),
+    return Form(
+      key: _formkey,
+      child: Scaffold(
+        appBar: AppBar(title: Text("Todo App ")),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Enter a text";
+                  }
+                },
 
-            Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: "Enter a name",
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
+                controller: _controller,
+                decoration: InputDecoration(
+                  hintText: "Enter a todo",
+                  border: OutlineInputBorder(),
                 ),
+              ),
+              SizedBox(height: 30),
 
-              ],
-            ),
-          ],
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Enter a name";
+                  }
+                },
+
+                controller: controller,
+                decoration: InputDecoration(
+                  hintText: "Enter a name",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+
+              SizedBox(height: 20),
+              ElevatedButton(onPressed: () {
+                if (_formkey.currentState!.validate()){
+                  FirebaseFirestore.instance.collection("Todo").add({
+                    "name" :_controller.text,
+                    "text": controller.text,
+                  });
+                  print("sucsessfullY");
+                  Navigator.of(context).pop();
+
+
+                }
+              }, child: Text("Add")),
+            ],
+          ),
         ),
       ),
     );
